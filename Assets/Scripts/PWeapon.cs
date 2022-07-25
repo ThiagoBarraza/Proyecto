20b73e964923b0ccac1efa8d8e6cc185ccc9fa45
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-07-25 20:21:10 -0300

    Bullet Collider Modified
    From Sphere to Box
    The mech now "walks"

--- Assets/Scripts/PWeapon.cs
+++ Assets/Scripts/PWeapon.cs
@@ -24,8 +24,6 @@ public class PWeapon : MonoBehaviour
         {
             var b = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
             b.transform.eulerAngles += new Vector3(Random.Range(-inacuracy, inacuracy), Random.Range(-inacuracy, inacuracy), Random.Range(-inacuracy, inacuracy));
-            //esta linea esta por si quieren hacer que la bala tenga cierta impresicion
-            //en su salida. Solo la roto en x y z al azar
             currReloadTime = reloadTime;
         }
     }
