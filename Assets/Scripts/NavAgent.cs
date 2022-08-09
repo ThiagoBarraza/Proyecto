44abb4f77e57be8555a5d641a96cde698a2769ac
ThiagoBarraza <101569153+ThiagoBarraza@users.noreply.github.com>
2022-08-09 14:43:10 -0300

    Artly can now follow you
    It will follow you and beat you to death

---
+++ Assets/Scripts/NavAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgent : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+    public Transform Target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.destination = Target.position;
+    }
+}
