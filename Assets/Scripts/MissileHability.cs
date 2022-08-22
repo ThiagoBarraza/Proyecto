a546e0f84e4da6604304b5af90c778b1c9b55fcf
ThiagoBarraza <101569153+ThiagoBarraza@users.noreply.github.com>
2022-08-22 11:40:58 -0300

    Working on the missiles hability

---
+++ Assets/Scripts/MissileHability.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileHability : MonoBehaviour
+{
+    [SerializeField] GameObject Missile;
+    [SerializeField] Transform[] MissileSpawn;
+    [SerializeField] float Cooldown;
+    public CameraTest CT;
+    public Vector3 Target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CT = FindObjectOfType<CameraTest>();
+
+
+        for(int i = 0; i < MissileSpawn.Length; i++)
+        {
+            var b = Instantiate(Missile, MissileSpawn[i].position, MissileSpawn[i].rotation);
+        }
+
+
+        
+    }
+}
---
+++ Assets/Sprites/MissileController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missilecontroller : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    Vector3 Target;
+    GameObject Player;
+    public CameraTest CT;
+    public float RotationSpeed;
+    public float TrackTime;
+    public float StopTrack;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+        CT = FindObjectOfType<CameraTest>();
+        Target = CT.worldPosition;
+    }
+
+    void Update()
+    {
+
+        transform.position += transform.forward * speed;
+
+
+
+        if (TrackTime > 0)
+        {
+            TrackTime -= Time.deltaTime;
+        }
+        if (TrackTime <= 0 && StopTrack > 0)
+        {
+            StopTrack -= Time.deltaTime;
+            var targetRotation = Quaternion.LookRotation(Target - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Ground" && col.gameObject.tag != "Ammo" && col.gameObject.tag != "EnemyBullet" && col.gameObject.tag != "Rocket")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
