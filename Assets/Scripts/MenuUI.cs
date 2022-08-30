0d70022cf7a69abadf3b088c06cacb64546c6643
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-08-29 23:24:24 -0300

    Advanced on the menu, it works!, i mean it should, it was easy

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -5,10 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    [SerializeField] GameObject[] UIElements;
+    [SerializeField] GameObject[] LSUI;
+    //[SerializeField] GameObject[] SettingsUI;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIElements = GameObject.FindGameObjectsWithTag("MenuUI");
+        LSUI = GameObject.FindGameObjectsWithTag("LSUI");
+        for(int i = 0; i < LSUI.Length; i++)
+        {
+            LSUI[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -17,8 +27,20 @@ public class MenuUI : MonoBehaviour
         
     }
 
-    void LoadGame()
+    public void LoadGame(string Scene)
     {
-        SceneManager.LoadScene("Stage1_Space");
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void OpenLevelSelector(bool state)
+    {
+        for(int i = 0; i < UIElements.Length; i++)
+        {
+            UIElements[i].SetActive(state);
+        }
+        for(int i = 0; i < LSUI.Length; i++)
+        {
+            LSUI[i].SetActive(!state);
+        }
     }
 }
