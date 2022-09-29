a69f046dcc2e0e12a0a637db0701742d39a5ff70
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-09-29 16:18:39 -0300

    Water has an effect

---
+++ Assets/Scripts/water.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+    [SerializeField] Vector3 PosInicial;
+
+    
+    [SerializeField] float speed;
+
+    public void Update()
+    {
+        float y = Mathf.PingPong(Time.time * speed, 1) * -3;
+        gameObject.transform.position = new Vector3(PosInicial.x , y -1, PosInicial.z);
+    }
+}
