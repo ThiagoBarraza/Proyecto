323075853357bd439a0049d74a4af9b38eefc6fc
ThiagoBarraza <101569153+ThiagoBarraza@users.noreply.github.com>
2022-10-12 16:02:06 -0300

    door

---
+++ Assets/Scripts/ConditionDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionDoor : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] char axis;
+    [SerializeField] GameObject DoorL;
+    [SerializeField] Vector3 DoorLFinalPos;
+    [SerializeField] Vector3 DoorRFinalPos;
+    [SerializeField] GameObject DoorR;
+    [SerializeField] GameObject Trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Trigger)
+        {
+            OpenDoors();
+        }
+    }
+
+    void OpenDoors()
+    {
+        if (DoorL.transform.position.x > DoorLFinalPos.x)
+        {
+            DoorL.transform.Translate(-speed, 0, 0);
+        }
+        if (DoorR.transform.position.x > DoorRFinalPos.x)
+        {
+            DoorR.transform.Translate(speed, 0, 0);
+        }
+    }
+}
