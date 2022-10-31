7c282f008ff9c4defc6a8e0d0c8c81bd2b1206b3
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-10-31 08:26:08 -0300

    added some models

---
+++ Assets/ColDestroy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColDestroy : MonoBehaviour
+{
+    [SerializeField] GameObject GameBundle;
+    [SerializeField] ParticleSystem DeathSystem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if (DeathSystem)
+            {
+                Instantiate(DeathSystem, gameObject.transform);
+            }
+            
+            if (GameBundle)
+            {
+                Destroy(GameBundle);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
