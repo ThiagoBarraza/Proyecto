5e34086f408d47debcd81212ab65289df9cd855d
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-11-09 04:35:36 -0300

    Idk

--- Assets/Scripts/Destruction.cs
+++ Assets/Scripts/Destruction.cs
@@ -6,13 +6,29 @@ public class Destruction : MonoBehaviour
 {
     [SerializeField] float Time;
     [SerializeField] GameObject Trigg;
-    
+    [SerializeField] bool HasAudio;
+    private AudioSource Sound;
+    private bool Played = false;
+
     // Update is called once per frame
+    void Start()
+    {
+        if (HasAudio)
+        {
+            Sound = GetComponent<AudioSource>();
+        }
+    }
+    
     void Update()
     {
         if (!Trigg)
         {
             Destroy(gameObject, Time);
+            if (Sound && !Played)
+            {
+                Sound.Play();
+                Played = true;
+            }
         }
         else
         {
