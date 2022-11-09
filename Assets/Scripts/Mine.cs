e47487eaaaa8c6b167bf2339b483af41bcde67d5
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-11-09 05:40:33 -0300

    Added mine

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    Animator Anim;
+    [SerializeField] float DetoTime;
+    [SerializeField] ParticleSystem DeathSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Anim.GetBool("Active") && DetoTime > 0)
+        {
+            DetoTime -= Time.time;
+        }
+        else if(Anim.GetBool("Active") && DetoTime < 0)
+        {
+            Instantiate(DeathSystem, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Anim.SetBool("Active", true);
+        }
+    }
+}
