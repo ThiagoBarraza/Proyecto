e4515025fee24f2cde419326911d3e58c549041b
ThiagoBarraza <101569153+ThiagoBarraza@users.noreply.github.com>
2022-11-10 14:32:34 -0300

    Music Mute

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource Sound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MuteMusic()
+    {
+        if(Sound.mute == true)
+        {
+            Sound.mute = true;
+        }
+        else
+        {
+            Sound.mute = false;
+        }
+    }
+}
